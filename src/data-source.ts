6aa3c5cc770f272103f0567b41e4abf9e47ee193
박민한 <alsgks1218@naver.com>
2022-08-30 17:21:47 +0900

    add server entity 작업

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,16 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: `${process.env.DB_HOST}`,
   port: 5432,
-  username: "runningwater",
-  password: "password",
-  database: "runningwater",
+  username: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_DATABASE}`,
   synchronize: true,
   logging: false,
   entities: ["src/entities/**/*.ts"],
